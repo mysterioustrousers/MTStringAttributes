9afc0b74783cbbe1da8d674432dfd79e255625bf
Adam Kirk <atomkirk@gmail.com>
2013-11-16 20:31:26 -0700

    adding all possible attributes on a first level

--- MTStringAttributes/MTStringAttributes.h
+++ MTStringAttributes/MTStringAttributes.h
@@ -10,32 +10,56 @@
 @interface MTStringAttributes : NSObject
 
 // Basics
-@property (strong, nonatomic) id                font;                   // UIFont || NSFont
-@property (strong, nonatomic) id                textColor;              // UIColor || NSColor
-@property (strong, nonatomic) id                backgroundColor;        // UIColor || NSColor
-@property (        nonatomic) BOOL              strikethrough;
-@property (        nonatomic) BOOL              underline;
+@property (nonatomic, strong) id                 font;                              // UIFont || NSFont
+@property (nonatomic, strong) id                 textColor;                         // UIColor || NSColor
+@property (nonatomic, strong) id                 backgroundColor;                   // UIColor || NSColor
 
-// Advanced
-@property (        nonatomic) BOOL              ligatures;              // Allow some characters to be combined
-@property (strong, nonatomic) NSNumber          *kern;                  // distance between characters
-@property (strong, nonatomic) id                outlineColor;           // UIColor || NSColor
-@property (strong, nonatomic) NSNumber          *outlineWidth;
+// More
+@property (nonatomic, assign) BOOL               ligatures;                         // Allow some characters to be combined
+@property (nonatomic, strong) CGFloat            *kern;                             // distance between characters
+@property (nonatomic, assign) BOOL               strikethrough;
+@property (nonatomic, strong) id                 strikethroughColor;
+@property (nonatomic, assign) BOOL               underline;
+@property (nonatomic, strong) id                 underlineColor;
+@property (nonatomic, strong) id                 strokeColor;
+@property (nonatomic, assign) CGFloat            strokeWidth;
+@property (nonatomic, strong) NSString           *textEffect;                      // nil or NSTextEffectLetterpressStyle
+@property (nonatomic, strong) NSTextAttachment   *textAttachment;
+@property (nonatomic, strong) NSURL              *link;
+@property (nonatomic, assign) CGFloat            baselineOffset;
+@property (nonatomic, assign) CGFloat            obliqueness;
+@property (nonatomic, assign) CGFloat            expansion;
+@property (nonatomic, assign) BOOL               verticalGlyphForm;
 
 // Paragraph Style
-@property (strong, nonatomic) NSParagraphStyle  *paragraphStyle;
+@property (nonatomic, assign) CGFloat            *lineSpacing;
+@property (nonatomic, assign) CGFloat            *paragraphSpacing;
+@property (nonatomic, assign) NSTextAlignment    alignment;
+@property (nonatomic, assign) CGFloat            firstLineHeadIndent;
+@property (nonatomic, assign) CGFloat            headIndent;
+@property (nonatomic, assign) CGFloat            tailIndent;
+@property (nonatomic, assign) NSLineBreakMode    lineBreakMode;
+@property (nonatomic, assign) CGFloat            minLineHeight;
+@property (nonatomic, assign) CGFloat            maxLineHeight;
+@property (nonatomic, strong) NSTextTab          *tabStop;
+@property (nonatomic, strong) NSTextTab          *removeTabStop;
+@property (nonatomic, strong) NSArray            *tabStops;
+@property (nonatomic, assign) NSWritingDirection writingDirection;
+@property (nonatomic, assign) CGFloat            lineHeightMultiple;
+@property (nonatomic, assign) CGFloat            paragraphSpacingBefore;
+@property (nonatomic, assign) CGFloat            defaultTabInterval;
+@property (nonatomic, strong) NSArray            *textBlocks;
+@property (nonatomic, strong) NSArray            *textLists;
+@property (nonatomic, assign) CGFloat            hyphenationFactor;
+@property (nonatomic, assign) CGFloat            tighteningFactorForTruncation;
+@property (nonatomic, assign) NSInteger          headerLevel;
 
 // Shadow
-@property (strong, nonatomic) NSNumber          *shadowBlurRadius;
-@property (strong, nonatomic) id                shadowColor;            // UIColor || NSColor
-@property (strong, nonatomic) NSNumber          *shadowOffsetX;
-@property (strong, nonatomic) NSNumber          *shadowOffsetY;
-
+@property (nonatomic, strong) CGFloat            *shadowBlurRadius;
+@property (nonatomic, strong) id                 shadowColor;                       // UIColor || NSColor
+@property (nonatomic, strong) CGFloat            *shadowOffsetX;
+@property (nonatomic, strong) CGFloat            *shadowOffsetY;
 
 - (NSDictionary *)dictionary;
-
-#if TARGET_OS_IPHONE
-- (NSDictionary *)UIKitDictionary;
-#endif
 
 @end
--- MTStringAttributes/MTStringAttributes.m
+++ MTStringAttributes/MTStringAttributes.m
@@ -8,109 +8,354 @@
 
 #import "MTStringAttributes.h"
 
+
+@interface MTStringAttributes ()
+@property (nonatomic, strong) NSMutableDictionary     *attributes;
+@property (nonatomic, strong) NSMutableParagraphStyle *paragraphStyle;
+@property (nonatomic, strong) NSShadow                *shadow;
+@end
+
+
 @implementation MTStringAttributes
 
+@dynamic lineSpacing;
+@dynamic paragraphSpacing;
+@dynamic alignment;
+@dynamic firstLineHeadIndent;
+@dynamic headIndent;
+@dynamic tailIndent;
+@dynamic lineBreakMode;
+@dynamic minLineHeight;
+@dynamic maxLineHeight;
+@dynamic *tabStop;
+@dynamic *removeTabStop;
+@dynamic *tabStops;
+@dynamic writingDirection;
+@dynamic lineHeightMultiple;
+@dynamic paragraphSpacingBefore;
+@dynamic defaultTabInterval;
+@dynamic *textBlocks;
+@dynamic *textLists;
+@dynamic hyphenationFactor;
+@dynamic tighteningFactorForTruncation;
+@dynamic headerLevel;
+@dynamic *shadowBlurRadius;
+@dynamic shadowColor;
+@dynamic *shadowOffsetX;
+@dynamic *shadowOffsetY;
 
 - (id)init
 {
     self = [super init];
     if (self) {
-        _strikethrough  = NO;
-        _underline      = NO;
-        _ligatures      = NO;
+        _attributes = [NSMutableDictionary new];
     }
     return self;
 }
 
+
+
+#pragma mark - Basics
+
+- (void)setFont:(id)font
+{
+    _font = font;
+    _attributes[NSFontAttributeName] = font;
+}
+
+- (void)setTextColor:(id)textColor
+{
+    _textColor = textColor;
+    _attributes[NSForegroundColorAttributeName] = textColor;
+}
+
+- (void)setBackgroundColor:(id)backgroundColor
+{
+    _backgroundColor = backgroundColor;
+    _attributes[NSBackgroundColorAttributeName] = backgroundColor;
+}
+
+
+
+
+
+#pragma mark - More
+
+- (void)setLigatures:(BOOL)ligatures
+{
+    _ligatures = ligatures;
+}
+
+- (void)setKern:(CGFloat)kern
+{
+    _kern = kern;
+    _attributes[NSKernAttributeName] = kern;
+}
+
+- (void)setStrikethrough:(BOOL)strikethrough
+{
+    _strikethrough = strikethrough;
+    _attributes[NSStrikethroughStyleAttributeName] = @(strikethrough);
+}
+
+- (void)setStrikethroughColor:(id)strikethroughColor
+{
+    _strikethroughColor = strikethroughColor;
+    _attributes[NSStrikethroughColorAttributeName] = strikethroughColor;
+}
+
+- (void)setUnderline:(BOOL)underline
+{
+    _underline = underline;
+    _attributes[NSUnderlineStyleAttributeName] = @(_underline);
+}
+
+- (void)setUnderlineColor:(id)underlineColor
+{
+    _underlineColor = underlineColor;
+    _attributes[NSUnderlineColorAttributeName] = underlineColor;
+}
+
+- (void)setStrokeColor:(id)strokeColor
+{
+    _strokeColor = strokeColor;
+    _attributes[NSStrokeColorAttributeName] = strokeColor;
+}
+
+- (void)setStrokeWidth:(CGFloat)strokeWidth
+{
+    _strokeWidth = strokeWidth;
+    _attributes[NSStrokeWidthAttributeName] = strokeWidth;
+}
+
+- (void)setTextEffect:(NSString *)textEffect
+{
+    _textEffect = textEffect;
+    _attributes[NSTextEffectAttributeName] = textEffect;
+}
+
+- (void)setTextAttachment:(NSTextAttachment *)textAttachment
+{
+    _textAttachment = textAttachment;
+    _attributes[NSAttachmentAttributeName] = textAttachment;
+}
+
+- (void)setLink:(NSURL *)link
+{
+    _link = link;
+    _attributes[NSLinkAttributeName] = link;
+}
+
+- (void)setBaselineOffset:(CGFloat)baselineOffset
+{
+    _baselineOffset = baselineOffset;
+    _attributes[NSBaselineOffsetAttributeName] = baselineOffset;
+}
+
+- (void)setObliqueness:(CGFloat)obliqueness
+{
+    _obliqueness = obliqueness;
+    _attributes[NSObliquenessAttributeName] = obliqueness;
+}
+
+- (void)setExpansion:(CGFloat)expansion
+{
+    _expansion = expansion;
+    _attributes[NSExpansionAttributeName] = expansion;
+}
+
+- (void)setVerticalGlyphForm:(BOOL)verticalGlyphForm
+{
+    _verticalGlyphForm = verticalGlyphForm;
+    _attributes[NSVerticalGlyphFormAttributeName] = verticalGlyphForm;
+}
+
+- (id)forwardingTargetForSelector:(SEL)aSelector
+{
+    if ([self.paragraphStyle respondsToSelector:aSelector]) {
+        return self.paragraphStyle;
+    }
+    else if ([self.shadow respondsToSelector:aSelector]) {
+        return self.shadow;
+    }
+    return [super forwardingTargetForSelector:aSelector];
+}
+
+
+
+//#pragma mark - Paragraph Style
+//
+//- (void)setLineSpacing:(CGFloat *)lineSpacing
+//{
+//    _lineSpacing = lineSpacing;
+//    self.paragraphStyle.lineSpacing = lineSpacing;
+//}
+//
+//- (void)setParagraphSpacing:(CGFloat *)paragraphSpacing
+//{
+//    _paragraphSpacing = paragraphSpacing;
+//    self.paragraphStyle.paragraphSpacing = paragraphSpacing;
+//}
+//
+//- (void)setAlignment:(NSTextAlignment)alignment
+//{
+//    _alignment = alignment;
+//    self.paragraphStyle.alignment = alignment;
+//}
+//
+//- (void)setFirstLineHeadIndent:(CGFloat)firstLineHeadIndent
+//{
+//    _firstLineHeadIndent = firstLineHeadIndent;
+//    self.paragraphStyle.firstLineHeadIndent = firstLineHeadIndent;
+//}
+//
+//- (void)setHeadIndent:(CGFloat)headIndent
+//{
+//    _headIndent = headIndent;
+//    self.paragraphStyle.headIndent = headIndent;
+//}
+//
+//- (void)setTailIndent:(CGFloat)tailIndent
+//{
+//    _tailIndent = tailIndent;
+//    self.paragraphStyle.tailIndent = tailIndent;
+//}
+//
+//- (void)setLineBreakMode:(NSLineBreakMode)lineBreakMode
+//{
+//    _lineBreakMode = lineBreakMode;
+//    self.paragraphStyle.lineBreakMode = lineBreakMode;
+//}
+//
+//- (void)setMinLineHeight:(CGFloat)minLineHeight
+//{
+//    _minLineHeight = minLineHeight;
+//    self.paragraphStyle.minimumLineHeight = minLineHeight;
+//}
+//
+//- (void)setMaxLineHeight:(CGFloat)maxLineHeight
+//{
+//    _maxLineHeight = maxLineHeight;
+//    self.paragraphStyle.maximumLineHeight = maxLineHeight;
+//}
+//
+//- (void)setTabStop:(NSTextTab *)tabStop
+//{
+//    _tabStop = tabStop;
+//    self.paragraphStyle
+//}
+//
+//- (void)setRemoveTabStop:(NSTextTab *)removeTabStop
+//{
+//
+//}
+//
+//- (void)setTabStops:(NSArray *)tabStops
+//{
+//
+//}
+//
+//- (void)setWritingDirection:(NSWritingDirection)writingDirection
+//{
+//
+//}
+//
+//- (void)setLineHeightMultiple:(CGFloat)lineHeightMultiple
+//{
+//
+//}
+//
+//- (void)setParagraphSpacingBefore:(CGFloat)paragraphSpacingBefore
+//{
+//
+//}
+//
+//- (void)setDefaultTabInterval:(CGFloat)defaultTabInterval
+//{
+//
+//}
+//
+//- (void)setTextBlocks:(NSArray *)textBlocks
+//{
+//
+//}
+//
+//- (void)setTextLists:(NSArray *)textLists
+//{
+//
+//}
+//
+//- (void)setHyphenationFactor:(CGFloat)hyphenationFactor
+//{
+//
+//}
+//
+//- (void)setTighteningFactorForTruncation:(CGFloat)tighteningFactorForTruncation
+//{
+//
+//}
+//
+//- (void)setHeaderLevel:(NSInteger)headerLevel
+//{
+//
+//}
+//
+//
+//
+//#pragma mark - Shadow
+//
+//- (void)setShadowBlurRadius:(CGFloat)shadowBlurRadius
+//{
+//
+//}
+//
+//- (void)setShadowColor:(id)shadowColor
+//{
+//
+//}
+//
+//- (void)setShadowOffsetX:(CGFloat)shadowOffsetX
+//{
+//
+//}
+//
+//- (void)setShadowOffsetY:(CGFloat)shadowOffsetY
+//{
+//    _shadowOffsetY = shadowOffsetY;
+//
+//}
+
+
 - (NSDictionary *)dictionary
 {
-    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
-
-    // Basics
-    if (_font) {
-        dictionary[NSFontAttributeName] = _font;
-    }
-
-    if (_textColor) {
-        dictionary[NSForegroundColorAttributeName] = _textColor;
-    }
-
-    if (_backgroundColor) {
-        dictionary[NSBackgroundColorAttributeName] = _backgroundColor;
-    }
-
-    if (_strikethrough) {
-        dictionary[NSStrikethroughStyleAttributeName] = @(_strikethrough);
-    }
-
-    if (_underline) {
-        dictionary[NSUnderlineStyleAttributeName] = @(_underline);
-    }
-
-
-
-    // Advanced
-    if (_ligatures) {
-        dictionary[NSLigatureAttributeName] = @(_ligatures);
-    }
-
-    if (_kern) {
-        dictionary[NSKernAttributeName] = _kern;
-    }
-
-    if (_outlineColor) {
-        dictionary[NSStrokeColorAttributeName] = _outlineColor;
-    }
-
-    if (_outlineWidth) {
-        dictionary[NSStrokeWidthAttributeName] = _outlineWidth;
-    }
-
-
-    // Paragraph Style
     if (_paragraphStyle) {
-        dictionary[NSParagraphStyleAttributeName] = _paragraphStyle;
+        self.attributes[NSParagraphStyleAttributeName] = self.paragraphStyle;
     }
-
-
-
-    // Shadow
-    if (_shadowBlurRadius || _shadowColor || _shadowOffsetX || _shadowOffsetY) {
-        NSShadow *shadow                        = [[NSShadow alloc] init];
-        if (_shadowBlurRadius)                  shadow.shadowBlurRadius = [_shadowBlurRadius floatValue];
-        if (_shadowColor)                       shadow.shadowColor      = _shadowColor;
-        if (_shadowOffsetX || _shadowOffsetY)   shadow.shadowOffset     = CGSizeMake([_shadowOffsetX floatValue], [_shadowOffsetY floatValue]);
-        dictionary[NSShadowAttributeName] = shadow;
+    if (_shadow) {
+        self.attributes[NSShadowAttributeName] = self.shadow;
     }
-
-    return dictionary;
+    return [self.attributes copy];
 }
 
-#if TARGET_OS_IPHONE
 
-- (NSDictionary *)UIKitDictionary
+
+
+#pragma mark - Private
+
+- (NSMutableParagraphStyle *)paragraphStyle
 {
-    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
-
-    if (_font) {
-        dictionary[UITextAttributeFont] = _font;
+    if (!_paragraphStyle) {
+        _paragraphStyle = [NSMutableParagraphStyle defaultParagraphStyle];
     }
-
-    if (_textColor) {
-        dictionary[UITextAttributeTextColor] = _textColor;
-    }
-
-    if (_shadowColor) {
-        dictionary[UITextAttributeTextShadowColor] = _shadowColor;
-    }
-
-    if (_shadowOffsetX || _shadowOffsetY) {
-        dictionary[UITextAttributeTextShadowOffset] = [NSValue valueWithUIOffset:UIOffsetMake([_shadowOffsetX floatValue], [_shadowOffsetY floatValue])];
-    }
-
-    return dictionary;
+    return _paragraphStyle;
 }
 
-#endif
+- (NSShadow *)shadow
+{
+    if (!_shadow) {
+        _shadow = [NSShadow new];
+    }
+    return _shadow;
+}
 
 @end
